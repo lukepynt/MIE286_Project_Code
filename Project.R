d3fad0a07f799ea8e94161562a4de51fcda5633e
Nick Fry <nicholasmtfry@gmail.com>
2026-04-07 12:09:42 -0400

    Update Project.R

--- Project.R
+++ Project.R
@@ -10,22 +10,22 @@ t0 <- feedback2$V2
 
 qqplot(acc1, rt(300, df = 5))
 
-qqnorm(acc1, main = "Normal Q-Q Plot Percentage Accuracy",
-       xlab = "Theoretical Quantiles", ylab = "Sample Quantiles",
+qqnorm(acc1, main = "Normal Q-Q Plot: Percentage Accuracy",
+       xlab = "Theoretical Quantiles", ylab = "Accuracy Quantiles",
        plot.it = TRUE, datax = FALSE); qqline(acc1, col = 2)
 
 qqplot(t1, rt(300, df = 5))
-qqnorm(t1, main = "Normal Q-Q Plot Percentage Time",
-       xlab = "Theoretical Quantiles", ylab = "Sample Quantiles",
+qqnorm(t1, main = "Normal Q-Q Plot: Percentage Time",
+       xlab = "Theoretical Quantiles", ylab = "Time Quantiles",
        plot.it = TRUE, datax = FALSE); qqline(t1, col = 2)
 
 qqplot(acc0, rt(300, df = 5))
-qqnorm(acc0, main = "Normal Q-Q Plot Colour Accuracy",
-       xlab = "Theoretical Quantiles", ylab = "Sample Quantiles",
+qqnorm(acc0, main = "Normal Q-Q Plot: Colour Accuracy",
+       xlab = "Theoretical Quantiles", ylab = "Accuracy Quantiles",
        plot.it = TRUE, datax = FALSE); qqline(acc0, col = 2)
 
 qqplot(t0, rt(300, df = 5))
-qqnorm(t0, main = "Normal Q-Q Plot Colour Time",
+qqnorm(t0, main = "Normal Q-Q Plot: Colour Time",
        xlab = "Theoretical Quantiles", ylab = "Sample Quantiles",
        plot.it = TRUE, datax = FALSE); qqline(t0, col = 2)
 
@@ -43,30 +43,41 @@ var(t1)
 mean(t0)
 var(t0)
 
-N=20 #just chosen arbitrarily
-samp=rnorm(N)
-myTest=t.test(samp)
-tcrit=qt(0.025, df=(N-1))
-
-dum=seq(-3.5, 3.5, length=10^4)#For the plot
-
-plot(dum, dt(dum, df=(N-1)), type='l', xlab='t', ylab='f(t)')
-abline(v=myTest$statistic, lty=2)
-abline(v=tcrit, col='red', lty=2)
-abline(v=-tcrit, col='red', lty=2)
-
 
 #t tests
 
-t.test(acc1, acc0)
-t.test(t1, t0)
+ttacc <- t.test(acc1, acc0)
+ttt<-t.test(t1, t0)
+print(ttacc)
+print(ttt)
 
+# plot 2 t test
 library(gginference)
 library(mcStats)
-ggttest(t.test(acc1, acc0))
+ggttest(ttacc)
 #showT.Test(acc1, acc0) not as good
-ggttest(t.test(t1, t0))
+ggttest(ttt)
 
-var.test(acc1, acc0)
-var.test(t1, t0)
+# f tests 
+ftacc<-var.test(acc1, acc0)
+ftt<-var.test(t1, t0)
+print(ftacc)
+print(ftt)
+
+# plotting f dist
+
+fcrit <-qf(0.025, 11, 13)
+fcrit2<-qf(0.025, 11, 13, lower.tail = FALSE)
+curve(df(x, df1=11, df2=13), from=0, to=5, xlab='x', ylab='F(x)')
+abline(v=fcrit, col='red', lty=2,)
+abline(v=fcrit2, col='red', lty=2)
+abline(v=0.81134, col='blue')
+abline(v=0.84541, col='blue')
+text(0,-0.02, "Fcrit", col = "gray60", adj = c(0, -.1))
+text(3.25,-0.02, "Fcrit", col = "gray60", adj = c(0, -.1))
+text(1.05,0, "Test stat FB1 = 0.8454", col = "blue", adj = c(0, -.1), srt = 90)
+text(0.65,0, "Test stat FB0 = 0.8113", col = "blue", adj = c(0, -.1), srt = 90)
+
+
+
 
